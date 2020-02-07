8ace6ef1fff45bf4153048bfbae778602cfeac78
YuShaoSu <n270882@gmail.com>
2020-02-07 17:01:10 +0800

    config bux fix

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from app.configmodule import DevelopmentConfig
 def create_app():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s : %(message)s', datefmt='%m-%d %H:%M')
     app = Flask(__name__)
-    app.config.from_object(DevelopmentConfig)
+    app.config.from_object(DevelopmentConfig())
 
     with app.app_context():
         db.init_app(app)
